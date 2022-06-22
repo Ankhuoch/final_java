01050efb443d1b1a7a0f8fc07203d778958ff390
An Khuoch <sreykhuochan@gmail.come>
2022-06-22 10:00:06 +0700

    api

--- heroku-demo/app.js
+++ heroku-demo/app.js
@@ -5,6 +5,21 @@ app.get("", (req, res) => {
     res.send('Hello');
 })
 
+app.get('/api/users', (req, res) => {
+    res.json([
+        {
+            name: "khuoch",
+            dob: "11/01/1995",
+            address: "Somewhere"
+        },
+        {
+            name: "dara",
+            dob: "22/02/2022",
+            address: "pp"
+        }
+    ])
+})
+
 app.listen(port, () => {
     console.log('app is running');
 })
